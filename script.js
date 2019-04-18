b067c50c97060f6b7e60f60221473cc9077ea5de
WojtechK <wojtek7297@gmail.com>
2019-04-18 23:26:44 +0200

    finished game

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const cards = document.querySelectorAll(".memo-card");
 
 let isFlipped = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 function Rotate() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
     this.classList.toggle('flip');
 
     if (!isFlipped) {
@@ -17,7 +21,45 @@ function Rotate() {
         //cards march
         console.log(firstCard.dataset.framework);
         console.log(secondCard.dataset.framework);
+
+        CheckMatching();
     }
 }
+
+function CheckMatching() {
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        //it's a match
+        disableCards();
+    } else {
+        //not a match
+        lockBoard = true;
+        setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+             resetBoard();
+        }, 1000);
+    }
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', Rotate);
+    secondCard.removeEventListener('click', Rotate);
+
+    resetBoard();
+}
+
+function resetBoard() {
+   [hasFlippedCard, lockBoard] = [false, false];
+   [firstCard, secondCard] = [null, null];
+}
+
+//self-executed function right after it's definition
+(function shuffle() {
+    cards.forEach(card => {
+        let random = Math.floor(Math.random() * 12)
+        card.style.order = random;
+    });
+})();
+
 
 cards.forEach(card => card.addEventListener('click', Rotate))
